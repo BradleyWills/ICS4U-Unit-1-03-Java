29ebbf4e5749030b5e946e21d2d550e7e69eca88
Bradley Wills <bradley.wills@mths.ca>
2020-02-23 23:28:48 +0000

    fifth commit

--- Extra.java
+++ Extra.java
@@ -2,18 +2,19 @@
 import java.util.Scanner;
 
 /**
-* The EinsteinFormula program implements an application that calculates how much 
-* potential energy given the mass.
+* The Extra program implements an application that calculates how many 
+* kiloton bombs the potential energy of an object is given the mass.
 *
 * @author  Bradley Wills
 * @version 1.0
-* @since   2020-02-18 
+* @since   2020-02-23 
 */
 
-public class EinsteinFormula {
+public class Extra {
   // Create variables 
   static double mass;
   static double energy;
+  static double numOfBombs;
   
   /**
   * Calculates and outputs the amount of potential energy given the mass.
@@ -28,11 +29,13 @@ public class EinsteinFormula {
     try { 
       mass = userInput.nextDouble(); 
       if (mass >= 0) {
-        // Calculate the potential energy
+        // Calculate  the potential energy
         energy =  mass *  Math.pow(2.988 * Math.pow(10,8),2);
+        // Calculates the number of kiloton bombs
+        numOfBombs = energy / Math.pow(10,12) / 63 * 15;
         // Tells the user the potential energy and restates the mass
-        System.out.println("The potential energy of an object with a mass of " + mass + "kg is " 
-            + energy + " joules.");
+        System.out.println("An object that has a mass of " + mass 
+            + "kg has a potential energy equivalent to " + numOfBombs + " kiloton bombs.");
       } else {
         // Informs user their input is invalid
         System.out.println("Invalid Input");
